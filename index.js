ad232748b5392b26eda523274134ed5f51144c7a
ZerocartDeployments <147301969+ZerocartDeployments@users.noreply.github.com>
2024-02-25 21:07:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('This Works !!!!!');
 });
 app.listen(3000, function () {
   console.log('BasicNodeApp listening on port 3000!');
